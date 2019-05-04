c206b9a77b203ea54f0c74758a820c13543fd35e
Panu Asikanius <panu.asikanius@vincit.com>
2019-05-04 18:31:25 +0300

    Stop Live after 5 minutes if user changes tabs

--- src/stores/index.js
+++ src/stores/index.js
@@ -2,7 +2,28 @@ import Store from './Store';
 
 const store = new Store();
 
+let stopLiveTimer = null;
+function handleVisibilityChange() {
+  // Stop live if user changes tabs for over a specified time
+  if (document.hidden && store.isLive) {
+    const delay = 1000 * 60 * 5; // 5 minutes
+    stopLiveTimer = setTimeout(store.stopLive, delay);
+  }
+
+  // Cancel if user comes back before timer runs out
+  if (!document.hidden && stopLiveTimer !== null) {
+    clearTimeout(stopLiveTimer);
+    stopLiveTimer = null;
+  }
+}
+
 async function initStore() {
+  if (typeof document.addEventListener === 'undefined' || typeof document.hidden === 'undefined') {
+    console.error('Page Visibility API is not supported on this browser.');
+  } else {
+    document.addEventListener('visibilitychange', handleVisibilityChange, false);
+  }
+
   await store.checkLogin();
 }
 
